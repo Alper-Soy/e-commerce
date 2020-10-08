c21bd4e2e6aa518ca26e2451099dd750e2935078
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-08 21:07:20 +0300

    Adding ant design icons

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,31 +1,47 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
 import {
-  MailOutlined,
   AppstoreOutlined,
   SettingOutlined,
+  UserAddOutlined,
+  UserOutlined,
 } from '@ant-design/icons';
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState();
+  const [current, setCurrent] = useState('home');
 
   const handleClick = (e) => {
-    console.log('click', e);
+    // console.log('click', e);
     setCurrent(e.key);
   };
 
   return (
-    <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
-      <Menu.Item key='mail' icon={<MailOutlined />}>
-        Home
-      </Menu.Item>
-      <SubMenu icon={<SettingOutlined />} title='Register'>
-        <Menu.Item key='setting:1'>Option 1</Menu.Item>
-        <Menu.Item key='setting:2'>Option 2</Menu.Item>
-      </SubMenu>
-    </Menu>
+    <>
+      <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
+        <Item key='home' icon={<AppstoreOutlined />}>
+          HOME
+        </Item>
+
+        <Item
+          className='float-right'
+          key='register'
+          icon={<UserAddOutlined />}
+        >
+          Register
+        </Item>
+
+        <Item className='float-right' key='login' icon={<UserOutlined />}>
+          Login
+        </Item>
+
+        <SubMenu icon={<SettingOutlined />} title='Username'>
+          <Item key='setting:1'>Option 1</Item>
+          <Item key='setting:2'>Option 2</Item>
+        </SubMenu>
+      </Menu>
+    </>
   );
 };
 
