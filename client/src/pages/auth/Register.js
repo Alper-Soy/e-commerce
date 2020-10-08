3c1251bf493da2637758577d2f4d694383c8d110
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-08 23:37:50 +0300

    Adding Email Link to Signin

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,32 @@
 import React, { useState } from 'react';
+import { auth } from '../../firebase';
+import { toast, ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const Register = () => {
   const [email, setEmail] = useState('');
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const config = {
+      url: 'http://localhost:3000/register/complete',
+      handleCodeInApp: true,
+    };
+
+    try {
+      const response = await auth.sendSignInLinkToEmail(email, config);
+      console.log(response);
+      toast.success(
+        `Email is sent to ${email}. Click the link to complete your registration.`
+      );
+
+      window.localStorage.setItem('emailForRegistration', email);
+      setEmail('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const registerForm = () => (
     <form onSubmit={handleSubmit}>
@@ -28,6 +51,7 @@ const Register = () => {
       <div className='row'>
         <div className='col-md-6 offset-md-3'>
           <h4>Register</h4>
+          <ToastContainer />
           {registerForm()}
         </div>
       </div>
