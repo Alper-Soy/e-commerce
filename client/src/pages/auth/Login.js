a08da645a013eb0075131450fe252c0ae75a4e06
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-14 11:52:36 +0300

    Implementing signin with email and password

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,9 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-const Login = () => {
+import { Button } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
+
+import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
+
+const Login = ({ history }) => {
+  const [email, setEmail] = useState('alpersoy89@gmail.com');
+  const [password, setPassword] = useState('123456');
+  const [loading, setLoading] = useState(false);
+
+  let dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+
+      dispatch({
+        type: 'LOGGED_IN_USER',
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+
+      history.push('/');
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <input
+          type='email'
+          className='form-control'
+          placeholder='Email address'
+          autoFocus
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className='form-group'>
+        <input
+          type='password'
+          className='form-control'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <Button
+        type='primary'
+        shape='round'
+        className='mb-3 primary'
+        block
+        size='large'
+        icon={<MailOutlined />}
+        disabled={!email || password.length < 6}
+        onClick={handleSubmit}
+      >
+        Login with Email/Password
+      </Button>
+    </form>
+  );
+
   return (
-    <div>
-      <p>Login</p>
+    <div className='container p-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <h4>Login</h4>
+          {loginForm()}
+        </div>
+      </div>
     </div>
   );
 };
