de6f37301c37a2d35ef179856d49bc4e063403ef
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-21 10:50:09 +0300

    Adding admin check middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require('../firebase');
+const User = require('../models/user');
 
 exports.authCheck = async (req, res, next) => {
   try {
@@ -7,6 +8,21 @@ exports.authCheck = async (req, res, next) => {
       .verifyIdToken(req.headers.authtoken);
     // console.log(firebaseUser);
     req.user = firebaseUser;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ err: 'Invalid or expired token' });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+  try {
+    const adminUser = await User.findOne({ email });
+
+    if (adminUser.role !== 'admin')
+      return res.status(403).json({ err: 'Admin resource. Access denied!' });
+
     next();
   } catch (err) {
     console.log(err);
