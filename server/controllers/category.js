1c72ff98a8f8e40e382064bf1ae35dd95ebc22aa
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-21 15:46:44 +0300

    Adding create category controller

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,11 +1,24 @@
 const Category = require('../models/category');
+const slugify = require('slugify');
 
-exports.create = (req, res) => {};
+exports.create = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const category = new Category({ name, slug: slugify(name) });
 
-exports.list = (req, res) => {};
+    await category.save();
 
-exports.read = (req, res) => {};
+    return res.status(201).json(category);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json('Create category failed!');
+  }
+};
 
-exports.update = (req, res) => {};
+exports.list = async (req, res) => {};
 
-exports.remove = (req, res) => {};
+exports.read = async (req, res) => {};
+
+exports.update = async (req, res) => {};
+
+exports.remove = async (req, res) => {};
