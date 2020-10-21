01cac68164a07f4373eaac218fda8c3df2b2d206
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-21 17:49:57 +0300

    Adding category CRUD requests

---
+++ client/src/api/category.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { api } from '../config';
+
+export const getCategories = async () => await axios.get(`${api}/categories`);
+
+export const getCategory = async (slug) =>
+  await axios.get(`${api}/category/${slug}`);
+
+export const removeCategory = async (slug, authtoken) =>
+  await axios.delete(`${api}/category/${slug}`, {
+    headers: {
+      authtoken,
+    },
+  });
+
+export const updateCategory = async (slug, category, authtoken) =>
+  await axios.put(`${api}/category/${slug}`, category, {
+    headers: {
+      authtoken,
+    },
+  });
+
+export const createCategory = async (category, authtoken) =>
+  await axios.post(`${api}/category`, category, {
+    headers: {
+      authtoken,
+    },
+  });
