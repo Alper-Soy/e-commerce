45252127274fd0d8a8ac7ecdf9bf5f3711c1d5b6
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-26 20:02:49 +0300

    Adding default image ant icons and product description

--- client/src/components/cards/AdminProductCard.js
+++ client/src/components/cards/AdminProductCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import laptop from '../../images/laptop.png';
 import { Card } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
@@ -7,15 +9,24 @@ const AdminProductCard = ({ product }) => {
   const { title, description, images } = product;
   return (
     <Card
+      hoverable
+      style={{ width: 300 }}
       cover={
         <img
-          src={images && images.length ? images[0].url : ''}
-          style={{ height: '150px', objectFit: 'cover' }}
-          className='m-2'
+          src={images && images.length ? images[0].url : laptop}
+          style={{ objectFit: 'scale-down', height: '300px' }}
+          className='p-1  '
         />
       }
+      actions={[
+        <EditOutlined className='text-primary' />,
+        <DeleteOutlined className='text-danger' />,
+      ]}
     >
-      <Meta title={title} description={description} />
+      <Meta
+        title={title}
+        description={`${description && description.substring(0, 30)}...`}
+      />
     </Card>
   );
 };
