f45c0ddf3dc4caa111f519aac8ef822c8a9eb68a
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-10-26 21:15:43 +0300

    Adding get product on update page

--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -2,15 +2,52 @@ import React, { useEffect, useState } from 'react';
 import AdminNav from '../../../components/nav/AdminNav';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
-import { createProduct } from '../../../api/product';
-import ProductCreateForm from '../../../components/forms/ProductCreateForm';
+import { getProduct } from '../../../api/product';
 import FileUpload from '../../../components/forms/FileUpload';
 import { getCategories, getCategorySubs } from '../../../api/category';
 import { LoadingOutlined } from '@ant-design/icons';
 
+const initialState = {
+  title: '',
+  description: '',
+  price: '',
+  categories: [],
+  category: '',
+  subs: [],
+  shipping: '',
+  quantity: '',
+  images: [],
+  colors: ['Black', 'Brown', 'Silver', 'White', 'Blue'],
+  brands: [
+    'Apple',
+    'Samsung',
+    'Microsoft',
+    'Lenovo',
+    'ASUS',
+    'Hewlett Packard',
+  ],
+  color: '',
+  brand: '',
+};
+
 const ProductUpdate = ({ match }) => {
+  const [values, setValues] = useState(initialState);
+
   const { slug } = match.params;
   const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug)
+      .then((product) => {
+        // console.log('Single Product', product.data);
+        setValues({ ...values, ...product.data });
+      })
+      .catch();
+  };
 
   return (
     <div className='container-fluid'>
@@ -21,7 +58,6 @@ const ProductUpdate = ({ match }) => {
 
         <div className='col-md-10'>
           <h4>Product update</h4>
-          {JSON.stringify(slug)}
           <hr />
         </div>
       </div>
